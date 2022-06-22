e8d90c0d0e5de85b45eb1e0268a4ca66f3e158c4
luquisinger <matheus.779@hotmail.com>
2022-06-22 20:29:31 -0300

    1

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-alert("Olá mundo")
+/* var nome = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector('#exampleInputCountry');
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector('#exampleInputPassword');
+var photo = document.querySelector("#exampleInputPhoto");
+var admin = document.querySelector('#exampleInputAdmin');
+o forEach substitui tudo isso*/
+
+var fields = document.querySelectorAll('#form-user-create [name]');
+var user = {};
+
+
+fields.forEach(function(field, index){
+
+    if(field.name == "gender"){
+        
+        if(field.checked){
+            user[field.name] = field.value;
+        } else {
+            user[field.name] = field.value;
+        }
+    }
+});
+
+console.log(user);
