19ce49f442d8b26398748cd6652ca51c9d07eef1
luquisinger <matheus.779@hotmail.com>
2022-06-23 20:19:17 -0300

    4

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,14 +11,69 @@ class UserController {
 
     onSubmit(){
 
-        //let _this = this; -> com a arrow function é mais facil
-
         this.formEl.addEventListener("submit", event => {
 
             event.preventDefault();
 
-            this.addLine(this.getValues())
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
+            let values = this.getValues();
+
+            this.getPhoto().then(
+                (content) => {
+
+                    values.photo = content;
+
+                    this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
+                }, 
+                (e) => {
+                    console.error(e)
+                }
+            );
         
+        });
+
+    }
+
+    getPhoto(){
+
+        return new Promise((resolve, reject) => {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+
+                if (item.name === 'photo') {
+                    return item;
+                }
+
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) => {
+
+                reject(e);
+
+            };
+            
+            if(file){
+                fileReader.readAsDataURL(file);
+                } else{
+                    resolve('dist/img/boxed-bg.jpg');
+                }
         });
 
     }
@@ -35,9 +90,12 @@ class UserController {
                     user[field.name] = field.value
                 }
     
+            } else if (field.name == "admin"){
+
+                user[field.name] = field.checked;
+
             } else {
-    
-                user[field.name] = field.value
+                 user[field.name] = field.value;
     
             }
     
@@ -59,20 +117,25 @@ class UserController {
     
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-            <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
-        `;
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
+        <tr>
+            <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `;
+
+        this.tableEl.appendChild(tr); 
 
     }
 
-} //fecha a classe
+
+}
