471c0cb48d262f640d4be590f34def6f255c5c75
luquisinger <matheus.779@hotmail.com>
2022-06-23 14:23:31 -0300

    2

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
-/* var nome = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector('#exampleInputCountry');
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector('#exampleInputPassword');
-var photo = document.querySelector("#exampleInputPhoto");
-var admin = document.querySelector('#exampleInputAdmin');
-o forEach substitui tudo isso*/
-
-var fields = document.querySelectorAll('#form-user-create [name]');
+var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
+function addLine(dataUser) {
 
-fields.forEach(function(field, index){
+    console.log(dataUser);
 
-    if(field.name == "gender"){
-        
-        if(field.checked){
-            user[field.name] = field.value;
+    document.getElementById("table-users").innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `;
+
+}
+
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    fields.forEach(function(field, index){
+
+        if (field.name === "gender") {
+
+            if (field.checked) {
+                user[field.name] = field.value
+            }
+
         } else {
-            user[field.name] = field.value;
-        }
-    }
-});
 
-console.log(user);
+            user[field.name] = field.value
+
+        }
+
+    });
+
+    var objectUser = new User(
+        user.name, 
+        user.gender, 
+        user.birth, 
+        user.country, 
+        user.email, 
+        user.password, 
+        user.photo, 
+        user.admin
+    );
+    
+    addLine(objectUser);
+
+});
